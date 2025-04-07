406a2b1a3f7be300e74b808859beb09f94ae4e80
shaibu lawal yusuf <ahbiggielawal@yahoo.com>
2025-04-07 11:00:44 +0100

    initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img className="circle-img" src={props.imgUrl} alt={props.name} />
+      </div>
+      <div className="bottom">
+        <p className="info">{props.phone}</p>
+        <p className="info">{props.email}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
+// This component is a functional component that takes props as an argument.
+// It returns a JSX element that represents a card with the following structure:
---
+++ src/contacts.js
@@ -0,0 +1,29 @@
+const contacts = [
+  {
+    name: "Reddinton",
+    imgURL:
+      "https://i.pinimg.com/736x/c5/24/7d/c5247dcc20ce2077dd1fbfdafcd19afa.jpg",
+    phone: "+123 456 789",
+    email: "cc@red.com"
+  },
+  {
+    name: "Jack Bauer",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  }
+];
+export default contacts;
+// This file exports an array of contact objects.
+// Each object contains the following properties:
+// - name: The name of the contact.
+// - imgURL: The URL of the contact's image.
+// - phone: The contact's phone number.   
